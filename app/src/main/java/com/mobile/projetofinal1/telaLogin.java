44e9fda5a4c8839a45c5c78e93ad8e808d6b0966
Alessandra <carvalho.machado.alessandra@gmail.com>
2022-11-03 16:39:15 -0300

    Atualizado em 03/11 às 16:38

--- app/src/main/java/com/mobile/projetofinal1/telaLogin.java
+++ app/src/main/java/com/mobile/projetofinal1/telaLogin.java
@@ -2,9 +2,14 @@ package com.mobile.projetofinal1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class telaLogin extends AppCompatActivity {
+
+    private TextView text_cadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +18,21 @@ public class telaLogin extends AppCompatActivity {
 
         //esconde a barra azul da tela
         getSupportActionBar().hide();
+
+        IniciarComponentes();
+
+        text_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //cria a intenção de que ao clicar no text de criar cadastro seja direcionado para a tela de cadastro
+                Intent intent = new Intent(telaLogin.this,telaCadastro.class);
+                startActivity(intent);
+            }
+        });
+    }
+    //faz referencia ao id do botão text_cadastro que leva para a próxima tela
+    private void IniciarComponentes(){
+        text_cadastro = findViewById(R.id.text_cadastro);
     }
 }
---
+++ app/src/main/java/com/mobile/projetofinal1/telaUsuario.java
@@ -0,0 +1,16 @@
+package com.mobile.projetofinal1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class telaUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_usuario);
+
+        getSupportActionBar().hide();
+    }
+}
